76ab81214d0a5c8eee40254025f9b4fe4fc94506
papergliff <papergliff@gmail.com>
2018-06-17 18:52:24 +0300

    Greet new user, and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket = io()
 socket.on('connect', function () {
   console.log('Connected to server')
 
-  socket.emit('createMessage', {
-    from: 'papergliff',
-    text: 'Here is some funny app'
-  })
+  // socket.emit('createMessage', {
+  //   from: 'papergliff',
+  //   text: 'Here is some funny app'
+  // })
 })
 
 socket.on('disconnect', function () {
--- server/server.js
+++ server/server.js
@@ -15,9 +15,20 @@ app.use(express.static(publicPath))
 io.on('connection', socket => {
   console.log('New user connected')
 
-  socket.on('createMessage', message => {
-    console.log('newEmail', message)
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
 
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  })
+
+  socket.on('createMessage', message => {
+    console.log('newMessage', message)
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
