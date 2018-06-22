176287bdc36b1a9e48fceebc9561d6f8edfe49f7
papergliff <papergliff@gmail.com>
2018-06-22 14:16:15 +0300

    Add sharing geolocation support

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
 
-const { generateMessage } = require('./utils/message')
+const { generateMessage, generateLocationMessage } = require('./utils/message')
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
@@ -26,14 +26,14 @@ io.on('connection', socket => {
     callback('This is from the server')
   })
 
+  socket.on('createLocationMessage', (coords) => {
+    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude))
+  })
+
   socket.on('disconnect', () => {
     console.log('Client disconnected')
   })
 })
-
-// app.get('/', (req, res) => {
-//   res.send()
-// })
 
 server.listen(port, () => {
   console.log(`App is listening on ${port}`)
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,13 +1,25 @@
-const { generateMessage } = require('./message')
+const { generateMessage, generateLocationMessage } = require('./message')
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
-    const from = 'testUser'
-    const text = 'Hello text'
+    const from    = 'testUser'
+    const text    = 'Hello text'
     const message = generateMessage(from, text)
 
-    expect(typeof message).toBe('object')
-    expect(message).toMatchObject({from, text: 'Hello text'})
+    expect(message).toMatchObject({ from, text: 'Hello text' })
+    expect(typeof message.createdAt).toBe('number')
+  })
+})
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from      = 'testUser'
+    const latitude  = 45
+    const longitude = 41
+    const url       = 'https://google.com/maps?q=45,41'
+    const message   = generateLocationMessage(from, latitude, longitude)
+
+    expect(message).toMatchObject({ from, url })
     expect(typeof message.createdAt).toBe('number')
   })
 })
